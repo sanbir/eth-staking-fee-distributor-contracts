7c1d44dca5f3eb2b372b337c158a62864c70aea0
Alexander Biryukov <alexander.biryukov@p2p.org>
2023-04-20 17:53:13 +0300

    add StakefishNFTManager

--- P2pEth2Depositor_arguments.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    false,
-    "0x0000000000000000000000000000000000000000",
-    "0x7A4204cf477e01BF9E34BdA6592BD4392a787Cb7"
-];
-
-// needed for Etherscan verification
--- scripts/models/DepositData.ts
+++
@@ -1,6 +0,0 @@
-export interface DepositData {
-    pubkey: string
-    withdrawal_credentials: string
-    signature: string
-    deposit_data_root: string
-}
